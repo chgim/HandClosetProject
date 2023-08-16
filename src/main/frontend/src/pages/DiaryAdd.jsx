3eafb6a6a23a6afdfb14ed6e01c2f1384ac2ae75
Kim Chan Ho <chgim1128@gmail.com>
2023-08-16 23:38:54 +0900

    백엔드 로직 추가
    임시적으로 다이어리에 계절에 관련한 정보만 저장할 수 있도록 백엔드 로직 작성. 프론트 엔드 DiaryAdd 작성 후 테스트 요함

--- src/main/frontend/src/pages/DiaryAdd.jsx
+++ src/main/frontend/src/pages/DiaryAdd.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
+import back from "../images/back.png";
+import styled from "styled-components";
+import update from "../images/update.png";
 const DiaryAdd = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const searchParams = new URLSearchParams(location.search);
   // const selectedDate = searchParams.get("selectedDate");
   const selectedDateUTC = new Date(searchParams.get("selectedDate"));
@@ -11,14 +15,48 @@ const DiaryAdd = () => {
   const formattedDate = selectedDateUTC.toLocaleDateString("ko-KR", {
     timeZone: "UTC", // 선택한 시간대에 맞게 변경
   });
+  const handleSubmit = () => {
+    navigate(`/Diary`);
+  };
 
   return (
-    <div>
+    <Container>
+      <Header>
+        <BackButton onClick={() => navigate("/Closet")}>
+          <img src={back} alt="back" style={{ width: "34px" }} />
+        </BackButton>
+        <SubmitButton onClick={handleSubmit}>제출</SubmitButton>
+      </Header>
       {/*<h2>Add Diary Entry for: {selectedDate}</h2>*/}
       <h2>Add Diary Entry for: {formattedDate}</h2>
       {/* Diary 추가 폼 또는 컴포넌트를 표시할 수 있습니다. */}
-    </div>
+    </Container>
   );
 };
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  width: 100%;
+  height: 40px;
+  margin-bottom: 30px;
+`;
+
+const BackButton = styled.div`
+  margin-top: 23px;
+  margin-left: 9%;
+`;
+const SubmitButton = styled.div`
+  margin-top: 23px;
+  margin-right: 9%;
+  font-size: 20px;
+  font-weight: bold;
+`;
 export default DiaryAdd;
